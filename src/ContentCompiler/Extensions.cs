5669936f305406849ca33ad530eeacb30dfb47d5
Nathan Jervis <mirhagk@gmail.com>
2016-03-29 11:00:57 -0400

    Refactored out classes into files

---
+++ src/ContentCompiler/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentCompiler
+{
+    static class Extensions
+    {
+        public static int? GetInt(this string value)
+        {
+            int result;
+            if (int.TryParse(value, out result))
+                return result;
+            return null;
+        }
+    }
+}
