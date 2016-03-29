1c0287c40293f6a52c688b9aeb8ba3ff63a898a8
Nathan Jervis <mirhagk@gmail.com>
2016-03-29 12:35:36 -0400

    Decompiler.Decompile method now simply dispatches and calls individual methods to decompile each section

--- src/ContentCompiler/Decompiler.cs
+++ src/ContentCompiler/Decompiler.cs
@@ -14,28 +14,32 @@ namespace ContentCompiler
     {
         public void Decompile(ContentManager content)
         {
+            DecompileSchedules(content);
+            DecompilePortraits(content);
+        }
+        void DecompileSchedules(ContentManager content)
+        {
             foreach (var asset in GetGameAssetsIn<Dictionary<string, string>>(content, "characters\\schedules"))
             {
                 var schedule = Schedule.Decompile(asset);
 
                 File.WriteAllText(Path.Combine(content.RootDirectory, "characters\\schedules", asset.Filename) + ".json", JsonConvert.SerializeObject(schedule, Formatting.Indented));
             }
-            DecompilePortraits(content);
         }
-        static IEnumerable<GameAsset<T>> GetGameAssetsIn<T>(ContentManager content, string relativePath)
+        void DecompilePortraits(ContentManager content)
+        {
+            foreach (var asset in GetGameAssetsIn<Texture2D>(content, "portraits"))
+                using (var stream = File.Create(content.RootDirectory + "\\Portraits\\" + asset.Filename + ".png"))
+                    asset.Content.SaveAsPng(stream, asset.Content.Width, asset.Content.Height);
+        }
+        
+        IEnumerable<GameAsset<T>> GetGameAssetsIn<T>(ContentManager content, string relativePath)
         {
             var items = Directory.EnumerateFiles(Path.Combine(content.RootDirectory, relativePath)).Where(c => Path.GetExtension(c) == ".xnb").Select(c => Path.GetFileNameWithoutExtension(c));
             foreach (var item in items)
             {
                 yield return GameAsset.Create(item, content.Load<T>(Path.Combine(relativePath, item)));
             }
-        }
-
-        static void DecompilePortraits(ContentManager content)
-        {
-            foreach (var asset in GetGameAssetsIn<Texture2D>(content, "portraits"))
-                using (var stream = File.Create(content.RootDirectory + "\\Portraits\\" + asset.Filename + ".png"))
-                    asset.Content.SaveAsPng(stream, asset.Content.Width, asset.Content.Height);
         }
     }
 }
