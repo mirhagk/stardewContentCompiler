4c359e7a0a1634c17b46f767247e88ec9c859152
Nathan Jervis <mirhagk@gmail.com>
2016-03-29 15:26:19 -0400

    Start to decipher type specific quest info

--- src/ContentCompiler/Quest.cs
+++ src/ContentCompiler/Quest.cs
@@ -14,13 +14,24 @@ namespace ContentCompiler
             foreach(var pair in asset)
             {
                 var pieces = pair.Value.Split('/');
-                questMap.Add(pair.Key, new Quest()
+                var quest = new Quest()
                 {
                     Type = pieces[0],
                     Name = pieces[1],
                     Description = pieces[2],
                     Objective = pieces[3],
-                });
+                };
+                var typeSpecificInfo = pieces[4].Split(' ');
+                switch (quest.Type)
+                {
+                    case "Crafting":
+                        quest.ItemIDToCraft = int.Parse(typeSpecificInfo[0]);
+                        quest.ItemIsBigItem = typeSpecificInfo[1] == "true";
+                        break;
+                    default:
+                        break;
+                }
+                questMap.Add(pair.Key, quest);
             }
             return questMap;
         }
@@ -28,5 +39,7 @@ namespace ContentCompiler
         public string Name { get; set; }
         public string Description { get; set; }
         public string Objective { get; set; }
+        public int ItemIDToCraft { get; set; }
+        public bool ItemIsBigItem { get; set; }
     }
 }
